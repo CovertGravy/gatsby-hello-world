392889d4d0bb10209ca281a579160c669bf7fbd0
Jaunty5899 <38493892+Jaunty5899@users.noreply.github.com>
2018-04-20 13:47:57 +0530

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,6 +2,6 @@ import React from 'react';
 import Link from 'gatsby-link';
 
 export default () => 
-<div style={{background: 'lightgreen'; width:'900px';height: '500px'; position: relative}}>  
-        <div style={{background: 'lightblue'; width:'300px';height: '200px'; position: absolute;bottom: '0px';right:'0px'}}></div>
+<div style={{background: 'lightgreen'; width:'900px';height: '500px'; position: 'relative'}}>  
+        <div style={{background: 'lightblue'; width:'300px';height: '200px'; position: 'absolute';bottom: '0px';right:'0px'}}></div>
     </div>
