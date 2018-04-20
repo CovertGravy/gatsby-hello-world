dbbf6c543d0b22db50076762d01e10764dca797e
Ashish S <s.cricket.ashish@gmail.com>
2018-04-20 19:59:41 +0530

    box v0.3

--- src/components/box.js
+++ src/components/box.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default ({children}) => (
   <div style={{background: 'black', color: 'white', border: '2px outset white', width: 600, height: 400}}>
-
+    {children}
   </div>
 )
