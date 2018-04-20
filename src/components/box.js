8e9fafa4a54b9ff0c36394dc517801c3027e2ab3
Ashish S <s.cricket.ashish@gmail.com>
2018-04-20 20:56:59 +0530

    box v0.5 css added

--- src/components/box.js
+++ src/components/box.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default ({children}) => (
-  <div style={{background: 'teal', color: 'white', border: '2px outset black', width: 500, height: 300, maxWidth: 500}}>
+  <div style={{background: 'teal', color: 'white', border: '2px ridge black', width: 500, height: 300, maxWidth: 500}}>
     {children}
   </div>
 )
