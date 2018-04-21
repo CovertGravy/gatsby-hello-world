8e6c62d5531aa8444c3133b6c4c5e2fe6c8bf76a
Ashish S <s.cricket.ashish@gmail.com>
2018-04-21 21:26:45 +0530

    materialize-js-remove-import

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Link from "gatsby-link";
 import Box from "../components/box";
 import styles from "../style/index.module.css";
 import "../../node_modules/materialize-css/dist/css/materialize.css";
-import "../../node_modules/materialize-css/dist/js/materialize.js";
+// import "../../node_modules/materialize-css/dist/js/materialize.js";
 import {Button, Card, CardTitle} from "react-materialize";
 
 
